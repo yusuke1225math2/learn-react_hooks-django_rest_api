2539b7510ed18cce6e20997e3b9d9f531b83fe96
yusuke1225math2 <tambo@saiand.co.jp>
2024-05-02 16:54:23 +0900

    /api/tasks/ に対するget,deleteを実装

--- react-basic-drf/src/components/DrfApiFetch.js
+++ react-basic-drf/src/components/DrfApiFetch.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([])
+  const [selectedTask, setSelectedTask] = useState([])
+  const [id, setId] = useState(1)
 
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/tasks/', {
@@ -13,6 +15,25 @@ const DrfApiFetch = () => {
     .then(res => {setTasks(res.data)})
   }, [])
 
+  const getTask = () => {
+    axios.get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+      headers: {
+        'Authorization': 'Token c81294dab449be516d68dba5b9405625b4430afa'
+      }
+    })
+    .then(res => {setSelectedTask(res.data)})
+  }
+
+  const deleteTask = () => {
+    axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+      headers: {
+        'Authorization': 'Token c81294dab449be516d68dba5b9405625b4430afa'
+      }
+    })
+    .then(res => console.log(res))
+  }
+
+
   return (
     <div>
       <ul>
@@ -21,6 +42,12 @@ const DrfApiFetch = () => {
         }
       </ul>
 
+      Set id <br />
+      <input type='text' value={id} onChange={evt=>setId(evt.target.value)}/>
+      <br/>
+      <button type='button' onClick={()=>getTask()}>Get task</button>
+      <button type='button' onClick={()=>deleteTask()}>Delete</button>
+      <h3>{selectedTask.title}    {selectedTask.id}</h3>
     </div>
   )
 }
