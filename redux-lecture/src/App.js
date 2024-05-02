5012cf9336dd71e9d57f579f73b60fdc6810a5cd
yusuke1225math2 <tambo@saiand.co.jp>
2024-05-02 13:53:29 +0900

    reducerのcombineReducersを使って複数のreducerをひとまとめにしてそれぞれのreducerの状態管理をする

---
+++ redux-lecture/src/App.js
@@ -0,0 +1,32 @@
+import React, {useReducer} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+import rootReducer from './reducers/index';
+import {SELL_MEAT, SELL_VEGETABLE} from './reducers/actionTypes';
+import reducerMeat from './reducers/reducerMeat';
+import reducerVegetable from './reducers/reducerVegetable';
+
+function App() {
+
+  const initialState = {
+    reducerMeat: {numOfMeat: 30},
+    reducerVegetable: {numOfVegetable: 25}
+  }
+  const [state, dispatch] = useReducer(rootReducer, initialState)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={()=>dispatch({type: SELL_MEAT})}>Sell meat</button>
+        Today's Meat: {state.reducerMeat.numOfMeat}
+        <button onClick={()=>dispatch({type: SELL_VEGETABLE})}>Sell vegetable</button>
+        Today's Vegetable: {state.reducerVegetable.numOfVegetable}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
