c77b51d68691003d797ef5f5ed855fcbb69ec734
yusuke1225math2 <tambo@saiand.co.jp>
2024-05-02 17:42:49 +0900

    taskの追加・更新処理を実装

--- react-basic-drf/src/components/DrfApiFetch.js
+++ react-basic-drf/src/components/DrfApiFetch.js
@@ -5,6 +5,7 @@ const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([])
   const [selectedTask, setSelectedTask] = useState([])
   const [id, setId] = useState(1)
+  const [editedTask, setEditedTask] = useState({id:'', title:''})
 
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/tasks/', {
@@ -24,21 +25,63 @@ const DrfApiFetch = () => {
     .then(res => {setSelectedTask(res.data)})
   }
 
-  const deleteTask = () => {
+  const deleteTask = (id) => {
     axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
       headers: {
         'Authorization': 'Token c81294dab449be516d68dba5b9405625b4430afa'
       }
     })
-    .then(res => console.log(res))
+    .then(res => {setTasks(tasks.filter(task => task.id !== id)); setSelectedTask([])})
   }
 
+  const newTask = (task) => {
+    const data = {
+      title: task.title
+    }
+    axios.post(`http://127.0.0.1:8000/api/tasks/`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token c81294dab449be516d68dba5b9405625b4430afa',
+      }
+    })
+    .then(res => {
+      setTasks([...tasks, res.data]);
+      setEditedTask({id:'', title:''});
+    })
+  }
+
+  const editTask = (task) => {
+    axios.put(`http://127.0.0.1:8000/api/tasks/${task.id}/`, task, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token c81294dab449be516d68dba5b9405625b4430afa',
+      }
+    })
+    .then(res => {
+      setTasks(tasks.map(task => (task.id === editedTask.id ? res.data : task)));
+      setEditedTask({id:'', title:''});
+    })
+  }
+
+  const handleInputChange = () => evt => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setEditedTask({...editedTask, [name]:value});
+  }
 
   return (
     <div>
       <ul>
         {
-          tasks.map(task => <li key={task.id}>{task.title}    {task.id}</li>)
+          tasks.map(task =>
+          <li key={task.id}>{task.title}    {task.id}
+            <button type='button' onClick={()=>deleteTask(task.id)}>
+              <i className='fa fa-trash-alt'></i>
+            </button>
+            <button type='button' onClick={()=>setEditedTask(task)}>
+              <i className='fa fa-pen'></i>
+            </button>
+          </li>)
         }
       </ul>
 
@@ -46,8 +89,17 @@ const DrfApiFetch = () => {
       <input type='text' value={id} onChange={evt=>setId(evt.target.value)}/>
       <br/>
       <button type='button' onClick={()=>getTask()}>Get task</button>
-      <button type='button' onClick={()=>deleteTask()}>Delete</button>
+      {/* <button type='button' onClick={()=>deleteTask()}>Delete</button> */}
+
       <h3>{selectedTask.title}    {selectedTask.id}</h3>
+
+      <input type='text' name='title'
+       value={editedTask.title}
+       onChange={handleInputChange()}
+       placeholder='New task ?' required/>
+       {editedTask.id ?
+       <button type='button' onClick={()=>editTask(editedTask)}>Update</button> :
+       <button type='button' onClick={()=>newTask(editedTask)}>Create</button> }
     </div>
   )
 }
