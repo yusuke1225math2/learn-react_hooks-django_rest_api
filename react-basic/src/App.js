de32afb71e385e43d7cb076f2688b1c9e58d1745
yusuke1225math2 <tambo@saiand.co.jp>
2024-05-02 11:39:08 +0900

    useMemoを使ってみています

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -11,6 +11,7 @@ import BasicReducer from './components/BasicReducer';
 import CompB from './components/CompB';
 
 import {useReducer} from 'react'
+import Memo from './components/Memo';
 
 const initialState = 0
 const reducer = (currentState, action) => {
@@ -39,9 +40,9 @@ function App() {
         {/* <ApiFetch /> */}
         {/* <B /> */}
         {/* <BasicReducer /> */}
-        Count {count}
-        <CompB />
-
+        {/* Count {count} */}
+        {/* <CompB /> */}
+        <Memo />
 
       </header>
     </div>
