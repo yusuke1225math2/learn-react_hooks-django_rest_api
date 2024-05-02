fd4676c8a18f93f2146d4baeece00eca5e74c586
yusuke1225math2 <tambo@saiand.co.jp>
2024-05-02 11:05:11 +0900

    useContextで親から孫へpropsを経由せずに値を渡せるんだなぁ

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
 import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
+import B from './components/B';
 
 function App() {
   return (
+    <AppContext.Provider value={'value from App.js'}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,8 +18,11 @@ function App() {
         {/* <BasicUseEffect /> */}
         {/* <TimerContainer /> */}
         <ApiFetch />
+        <B />
+
       </header>
     </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ react-basic/src/components/B.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import C from './C'
+const B = () => {
+  return (
+    <div>
+      <h3>B</h3>
+      <C />
+    </div>
+  )
+}
+
+export default B
